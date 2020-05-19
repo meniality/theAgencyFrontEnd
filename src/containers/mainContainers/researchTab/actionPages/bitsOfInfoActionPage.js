61c075b4ddaedbaf3dd340b9f61ed3c4dded5b9e
meniality <patrh_99@hotmail.com>
2020-05-19 10:26:18 -0600

    completed min and max button on search the internet action.

--- src/containers/mainContainers/researchTab/actionPages/bitsOfInfoActionPage.js
+++ src/containers/mainContainers/researchTab/actionPages/bitsOfInfoActionPage.js
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 import actions from '../../../../actions'
-import {FaRegMinusSquare} from 'react-icons/fa'
+import {FaRegMinusSquare, FaRegPlusSquare} from 'react-icons/fa'
 import { IconContext } from "react-icons";
 
 const {resourcesActions: {incrementBitsOfInfoAction, incrementAlgorithmsAction, decrementMoneyAction, increaseAlgorithmCostAction}} = actions
@@ -25,10 +25,15 @@ function BitsOfInfoActionPage (props) {
     }
   }
 
-  const minimizedActionDiv = () => {
+  const minimizedActionDiv = (text, maximizeFunction) => {
     return (
-      <div style={styles.actionContainer}>
-        <p style = {styles.description}>test div</p>
+      <div style={styles.minimizedDiv}>
+        <p style = {styles.description}>{text}</p>
+        <IconContext.Provider value={{ color: "rgb(90, 90, 90)", className: "global-class-name" }}>
+          <div style={styles.minusButton}>
+            <FaRegPlusSquare onClick= {()=>{maximizeFunction()}}/>
+          </div>
+        </IconContext.Provider>
       </div>
     )
   }
@@ -48,15 +53,16 @@ function BitsOfInfoActionPage (props) {
   }
 
   const createBitsOfInfoActionTab = () => {
-    console.log(actionsVisibility)
     if(actionsVisibility.research.bitsOfInfo.searchTheInternet.minimized === false){
       return(
         <div style={styles.actionContainer}>
           <div style={styles.topDescriptionDiv}>
             <p style={styles.description}>Search the Internet for proof of the unknown</p>
-            <div style={styles.minusButton}>
-              <FaRegMinusSquare onClick= {()=>{props.toggleSearchTheInternetMinimized()}}/>
-            </div>
+              <IconContext.Provider value={{ color: "rgb(90, 90, 90)", className: "global-class-name" }}>
+                <div style={styles.minusButton}>
+                  <FaRegMinusSquare onClick= {()=>{props.toggleSearchTheInternetMinimized()}}/>
+                </div>
+              </IconContext.Provider>
           </div>
           <button className = {'button'}style = {styles.actionButton} 
             onClick ={()=>{
@@ -71,7 +77,8 @@ function BitsOfInfoActionPage (props) {
       )
     }
     else{
-      return minimizedActionDiv()
+      const text = "Search the internet for  Bits of Info"
+      return minimizedActionDiv(text, props.toggleSearchTheInternetMinimized)
     }
   }
 
@@ -145,7 +152,8 @@ const styles = {
   minusButton:{
     display: 'inline',
     height: 10,
-    float: 'right'
+    float: 'right',
+    marginTop: -5
   },
   title: {
     fontSize: 30,
@@ -175,5 +183,15 @@ const styles = {
     height: 35,
     borderRadius: 3,
     backgroundColor: 'rgba(128, 128, 128, 0.51)'
+  },
+  minimizedDiv:{
+    borderStyle: 'solid',
+    borderWidth: 1,
+    borderRadius: 3,
+    borderColor: 'rgba(105, 105, 105, 0.68)',
+    padding: '5px 0px 5px 0px',
+    width: '95%',
+    textAlign: 'center',
+
   }
 }
